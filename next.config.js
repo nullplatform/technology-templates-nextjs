375ecdef68af729d88a4035be89cbbde34e82a38
Pablo Vilas <pablovilas89@gmail.com>
2023-02-23 21:38:02 -0300

    Add health endpoint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  rewrites: async () => {
+    return [
+      {
+        source: '/health',
+        destination: '/api/health',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
